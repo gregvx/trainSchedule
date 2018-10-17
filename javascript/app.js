088656e6da02965c0888518443e1135ce4a323e4
gregvx <32096447+gregvx@users.noreply.github.com>
2018-10-17 02:14:59 -0600

    fixed arrival time column and added remarks to readme file.

--- javascript/app.js
+++ javascript/app.js
@@ -1,43 +1,19 @@
-
-// Assume the following situations.
-
-// (TEST 1)
-// First Train of the Day is 3:00 AM
-// Assume Train comes every 3 minutes.
-// Assume the current time is 3:16 AM....
-// What time would the next train be...? (Use your brain first)
-// It would be 3:18 -- 2 minutes away
-
-// (TEST 2)
-// First Train of the Day is 3:00 AM
-// Assume Train comes every 7 minutes.
-// Assume the current time is 3:16 AM....
-// What time would the next train be...? (Use your brain first)
-// It would be 3:21 -- 5 minutes away
-
-
-// ==========================================================
-
-
-
-
-// 1. Initialize Firebase
+// Initialize Firebase
 var config = {
     apiKey: "AIzaSyCcSBS6ysfWBG2K77eaqkzrrB60nlS4b9s",
     authDomain: "trainscheduler-dd560.firebaseapp.com",
     databaseURL: "https://trainscheduler-dd560.firebaseio.com/",
     storageBucket: "trainscheduler-dd560.appspot.com"
 };
-
 firebase.initializeApp(config);
-
 var database = firebase.database();
 
-// 2. Button for adding Routes
+
+// Button for adding Routes
 $("#add-train-btn").on("click", function (event) {
     event.preventDefault();
 
-    // Grabs user input
+    // Grab user input
     var trainName = $("#train-name-input").val().trim();
     var trainDest = $("#destination-input").val().trim();
     var trainFirst = moment($("#first-time-input").val().trim(), "HH:mm").format("X");
@@ -60,6 +36,7 @@ $("#add-train-btn").on("click", function (event) {
     console.log(newTrain.destination);
     console.log(newTrain.firstTime);
     console.log(newTrain.frequency);
+    
 
     // alert("Route successfully added");
 
@@ -70,10 +47,10 @@ $("#add-train-btn").on("click", function (event) {
     $("#frequency-input").val("");
 });
 
-// 3. Create Firebase event for adding route to the database and a row in the html when a user adds an entry
+// Create Firebase event for adding route to the database and a row in the html when a user adds an entry
 database.ref().on("child_added", function (childSnapshot) {
-    console.log("We just got word from the database that data was added. Here is what the database returned:");
-    console.log(childSnapshot.val());
+    //console.log("We just got word from the database that data was added. Here is what the database returned:");
+    //console.log(childSnapshot.val());
 
     // Store everything into a variable.
     var trainName = childSnapshot.val().name;
@@ -82,77 +59,49 @@ database.ref().on("child_added", function (childSnapshot) {
     var trainFreq = childSnapshot.val().frequency;
 
     // Route Info
-    console.log("Here is what the database returned in a more parsed out way:");
-    console.log(trainName);
-    console.log(trainDest);
-    console.log(trainFirst);
-    console.log(trainFreq);
+    // console.log("Note that train " + trainName + " has a start time of " + moment(trainFirst, "X").format("HH:mm") + " and a frquency of " + trainFreq);
 
     //Calculate time of next arrival
-    var trainNext = 5;
-    //TODO figure out how to do this
+    var currentTimeObject = moment();
+    var startTimeObject = moment(trainFirst, "X");
+    var firstMin = minutesFromMidnight(startTimeObject);
+    var currentMin = minutesFromMidnight(currentTimeObject);
+    var timeDiff;
+    if (currentMin >= firstMin) {
+        timeDiff = currentMin - firstMin;
+    }
+    else {
+        // say currentMin = 2 (00:02) and firstMin = 60 (01:00), the diff woulf be 23hours and 2 min or 23*60+2
+        timeDiff = (1440 - firstMin) + currentMin;  
+    }
 
-    // Solved Mathematically
-    // Test case 1:
-    // 16 - 00 = 16
-    // 16 % 3 = 1 (Modulus is the remainder)
-    // 3 - 1 = 2 minutes away
-    // 2 + 3:16 = 3:18
+    var minSinceLastTrain = timeDiff%trainFreq
+    var trainNext = parseInt(trainFreq) - minSinceLastTrain;
+    console.log("It is currently " + moment().format("HH:mm") +
+        ". So, " + timeDiff + " minutes have elapsed since the first train on the " + 
+        trainName + " route. Since the frequency is " +
+        trainFreq + " minutes, it must have been " + minSinceLastTrain + 
+        " minutes since the last train came through. The next train should therefore be in " +
+        trainNext + " minutes.");
+    // there is a rare case where the minutes to the next train arrival occurs after the first train of the day
+    // we need to check for this and show the number of minutes until the first train of the day.
+    //example: say we have a route that starts at 1:00am and goes comes again every 59 minutes...
+    //there woule be a train at 1:59, 2:58, 3:57, 4:56...23:37, 00:36. If the current time is 12:40am,
+    //the last train would have been 4 minutes ago at 12:36am, but the next train will not be 59 minutes later
+    //at 1:35am. It will be at 1:00am because that is when the new day's train shcedule starts, so
+    //at 12:40am, the next train will not be in 55 minutes, it will be in 20 minutes.
+    //Here, we fix this issue:
+    if((parseInt(currentMin) < parseInt(firstMin)) && (parseInt(currentMin)+parseInt(trainNext) > parseInt(firstMin))) {
+        trainNext = parseInt(firstMin) - parseInt(currentMin);
+        console.log("The weird case apllies here, so next train will actually be in " + trainNext + " minutes.");
+    }
 
-    // Solved Mathematically
-    // Test case 2:
-    // 16 - 00 = 16
-    // 16 % 7 = 2 (Modulus is the remainder)
-    // 7 - 2 = 5 minutes away
-    // 5 + 3:16 = 3:21
-
-    // Assumptions
-    var tFrequency = 3;
-
-    // Time is 3:30 AM
-    var firstTime = "03:30";
-
-    // First Time (pushed back 1 year to make sure it comes before current time)
-    var firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
-    // console.log(firstTimeConverted);
-
-    // Current Time
-    var currentTime = moment();
-    // console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
-
-    // Difference between the times
-    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
-    // console.log("DIFFERENCE IN TIME: " + diffTime);
-
-    // Time apart (remainder)
-    var tRemainder = diffTime % tFrequency;
-    // console.log(tRemainder);
-
-    // Minute Until Train
-    var tMinutesTillTrain = tFrequency - tRemainder;
-    // console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
-
-    // Next Train
-    var nextTrain = moment().add(tMinutesTillTrain, "minutes");
-    // console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
-
-    // Prettify the first train start time
-    // var empStartPretty = moment.unix(empStart).format("MM/DD/YYYY");
-
-    // Calculate the months worked using hardcore math
-    // To calculate the months worked
-    // var empMonths = moment().diff(moment(empStart, "X"), "months");
-    // console.log(empMonths);
-
-    // Calculate the total billed rate
-    // var empBilled = empMonths * empRate;
-    // console.log(empBilled);
 
     // Create the new row
     var newRow = $("<tr>").append(
         $("<td>").text(trainName),
         $("<td>").text(trainDest),
-        $("<td>").text(trainFirst),
+        $("<td>").text(moment(trainFirst, "X").format("HH:mm")),
         $("<td>").text(trainFreq),
         $("<td>").text(trainNext)
     );
@@ -160,3 +109,16 @@ database.ref().on("child_added", function (childSnapshot) {
     // Append the new row to the table
     $("#route-table > tbody").append(newRow);
 });
+
+//this function takes moment object and returns the number of minutes elapsed since midnight.
+function minutesFromMidnight(mo) {
+    // console.log("So we have a time that is stored as " + mo + " but the they time in military format is " + mo.format("HH:mm"));
+    var hours = mo.format("H");
+    var mins = mo.format("m");
+    var elapsed = 60*parseInt(hours) + parseInt(mins);
+    return elapsed;
+    // console.log("The hours on that time were " + hours + " and the minutes were " + mins);
+    // console.log("So, the total minutes elapsed since midnight have been " + elapsed);
+    // console.log("More formally, the time sent to the database could be extressed as " + moment(firstTime, "X").format("MM-DD-YYYY HH:mm"));
+    // console.log("The current time is " + moment() + " or " + moment().format("HH:mm"));
+}
